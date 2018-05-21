6d9e152a14d4387aa37a075866f5dea64f7e798b
Spellacy, Michael <spell@spellacy.net>
2018-05-21 13:24:40 -0400

    Test

--- gdpr/qa.js
+++ gdpr/qa.js
@@ -324,7 +324,7 @@
 
   // Only load banner if banner has NEVER been displayed before.
 
-  if(bannerDisplayed !== null && consentCapture === null) {
+  if(bannerDisplayed !== null || consentCapture === null) {
 
     // Create Alert
 
