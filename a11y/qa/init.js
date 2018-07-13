bf25e89a4fcd8e3ebfc92be6e31f67265dd61316
Spellacy, Michael <spell@spellacy.net>
2018-07-13 18:17:06 -0400

    hjhjh

--- a11y/qa/init.js
+++ a11y/qa/init.js
@@ -94,6 +94,22 @@
 
     fixDisabledButton();
 
+    // Save Job for later
+
+    var savedJobs = document.querySelectorAll("[data-a11y-saved-button]");
+
+    for (var i = 0; i < savedJobs.length; i++) {
+
+      savedJobs[i].setAttribute("aria-expanded", "false");
+
+      savedJobs[i].addEventListener("click", function() {
+
+        alert("Test");
+
+      });
+
+    }
+
   });
 
   // Sometime we can only do things after TB has finished (slowly) loading it's portion of the DOM.
@@ -166,22 +182,6 @@
     // Add title to spinner
 
     $(".goog-te-spinner").append("<title>Spinner</title>");
-
-    // Save Job for later
-
-    var savedJobs = document.querySelectorAll("[data-a11y-saved-button]");
-
-    for (var i = 0; i < savedJobs.length; i++) {
-
-      savedJobs[i].setAttribute("aria-expanded", "false");
-
-      savedJobs[i].addEventListener("click", function() {
-
-        alert("Test");
-
-      });
-
-    }
 
   }, 1000);
 
