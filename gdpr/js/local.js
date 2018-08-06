e75e4fb6a0e5d1dbde41108c638c462b3f183338
Spellacy, Michael <spell@spellacy.net>
2018-08-06 14:39:31 -0400

    Added a TOC

--- gdpr/js/local.js
+++ gdpr/js/local.js
@@ -5,3 +5,15 @@ $(".code").each(function() {
   $(this).parent().load($(this).attr("href"));
 
 });
+
+// Quick & Dirty TOC
+
+$("#implementation").before("<h2>Table of Contents</h2><p>The following is a list of all the parameters that can currently be passed into the GDPR script.</p><ol id='toc'></ol>")
+
+$("h3[id*=data-]").each(function(index) {
+
+    var newAppendixItem = "<li><a href=#" + $(this).attr("id") + ">" + $(this).text() + "</a></li>";
+
+    $("#toc").append(newAppendixItem);
+
+});
