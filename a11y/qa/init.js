3f73955ab4b9ee66cc57f7e763bbc8b18fd84adb
Spellacy, Michael <spell@spellacy.net>
2018-12-06 20:44:31 -0500

    ghgghg

--- a11y/qa/init.js
+++ a11y/qa/init.js
@@ -169,7 +169,6 @@
 
 	  $(".data-form .form-field.required, .form-field.required input:not([type='checkbox']), .form-field.required select, .form-field.required textarea").removeAttr("aria-required");
 
-
   });
 
   // Sometime we can only do things after TB has finished (slowly) loading it's portion of the DOM.
@@ -178,6 +177,9 @@
 
 	  // A11y Form Fixes
 
+    // Issue: Remove aria-required from p element (it should not exist on this element) and various other elements.
+
+	  $(".data-form .form-field.required, .form-field.required input:not([type='checkbox']), .form-field.required select, .form-field.required textarea").removeAttr("aria-required");
 
 	  // Required="required" is XHTML serialization and may throw a11y validation issues if not set to blank or true.
 
