3cd9f675afe744fa25170e89f3d3378b9e225e1c
Spellacy, Michael <spell@spellacy.net>
2019-04-04 10:40:15 -0400

    Test

--- a11y/qa/init.js
+++ a11y/qa/init.js
@@ -94,13 +94,11 @@
 
     // While we are in here, let's indicate to screen readers what page they are on - cool!
 
-    //if (pageStatus !== null || pageStatus !== "undefined "){
+    if (pageStatus !== null || pageStatus !== "undefined "){
 
       $("#search-results").attr("aria-label", pageStatus);
 
-      console.log(pageStatus);
-
-    //}
+    }
 
     if($(".pagination-no-form").length) {
 
