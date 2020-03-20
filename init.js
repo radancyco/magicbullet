e16b411d47e40e02c70bccf44b2bb2a45479894e
Spellacy, Michael <spell@spellacy.net>
2020-03-20 13:32:17 -0400

    changed github.io to tmpworldwide.dev

--- init.js
+++ init.js
@@ -46,7 +46,7 @@
   hostName = hostName.substring(hostName.indexOf(".") + 1);
 
   var localPaths = localHost === "localhost";
-  var testPaths = hostName === "runmytests.com" || hostName === "talentbrew.com" || hostName === "github.io";
+  var testPaths = hostName === "runmytests.com" || hostName === "talentbrew.com" || hostName === "tmpworldwide.dev";
 
   // Check to see if both the GDPR and CCPA attributes exist together.
 
