5a917121c8dd8ac9f863917a0d72918faa8334a8
michaelspellacy <spell@spellacy.net>
2021-11-15 15:05:27 -0500

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -159,6 +159,84 @@
 
   });
 
+  // Issue: The Job Search custom datalist is really horrible. Now that IE11 is not supported, we should begin using datalist instead.
+
+  if(a11ySearchDataList === null) {
+
+    a11yBody.classList.add("a11y-search-location");
+
+    var $searchLocation = $(".search-location");
+    var $mindReaderResults = $(".mindreader-results");
+
+    $searchLocation.attr("list", "search-location-datalist").after("<datalist id='search-location-datalist' class='search-location-datalist'></div>");
+
+    var $searchLocationDatalist = $(".search-location-datalist");
+
+    $searchLocation.keypress(function() {
+
+      var $htmlStr = $mindReaderResults.find("a");
+      var $cloneList = $htmlStr.clone();
+
+      $searchLocationDatalist.html($cloneList);
+
+      var newList = $searchLocationDatalist.find("a");
+
+      newList.each(function(){
+
+        var $targetId = $(this).data("target-id");
+        var $lat = $(this).data("lat");
+        var $lon = $(this).data("lon");
+        var $lp = $(this).data("lp");
+        var $lt = $(this).data("lt");
+        var $pc = $(this).data("pc");
+        var $htmlText = $(this).text();
+        $(this).replaceWith("<option data-lat=" + $lat + " data-lon=" + $lon + " data-lp=" + $lp + " data-lt=" + $lt + " data-pc=" + $pc + ">" + $htmlText + "</option>");
+
+      });
+
+    });
+
+    $searchLocation.change(function() {
+
+      var val = $(this).val();
+
+      var lat = $searchLocationDatalist.find("option").filter(function() {
+
+        return this.value == val;
+
+      }).data("lat");
+
+      var lon = $searchLocationDatalist.find("option").filter(function() {
+
+        return this.value == val;
+
+      }).data("lon");
+
+      var lp = $searchLocationDatalist.find("option").filter(function() {
+
+        return this.value == val;
+
+      }).data("lp");
+
+      var lt = $searchLocationDatalist.find("option").filter(function() {
+
+        return this.value == val;
+
+      }).data("lt");
+
+      $(this).attr({
+
+        "data-lon": lon,
+        "data-lat": lat,
+        "data-lp": lp,
+        "data-lt": lt
+
+      });
+
+    });
+
+  }
+
   // Issue: Search Results pagination disabled button can be tabbed to (this is bad). To address this, we simply remove href. When removed, aria-hidden is not really needed, so we reove that, too!
 
   function fixDisabledButton() {
@@ -417,84 +495,6 @@
       $(".job-list .location, .job-list .date").each(function() {
 
         $(this).appendTo($(this).prev());
-
-      });
-
-    }
-
-    // Issue: The Job Search custom datalist is really horrible. Now that IE11 is not supported, we should begin using datalist instead.
-
-    if(a11ySearchDataList === null) {
-
-      a11yBody.classList.add("a11y-search-location");
-
-      var $searchLocation = $(".search-location");
-      var $mindReaderResults = $(".mindreader-results");
-
-      $searchLocation.attr("list", "search-location-datalist").after("<datalist id='search-location-datalist' class='search-location-datalist'></div>");
-
-      var $searchLocationDatalist = $(".search-location-datalist");
-
-      $searchLocation.keypress(function() {
-
-        var $htmlStr = $mindReaderResults.find("a");
-        var $cloneList = $htmlStr.clone();
-
-        $searchLocationDatalist.html($cloneList);
-
-        var newList = $searchLocationDatalist.find("a");
-
-        newList.each(function(){
-
-          var $targetId = $(this).data("target-id");
-          var $lat = $(this).data("lat");
-          var $lon = $(this).data("lon");
-          var $lp = $(this).data("lp");
-          var $lt = $(this).data("lt");
-          var $pc = $(this).data("pc");
-          var $htmlText = $(this).text();
-          $(this).replaceWith("<option data-lat=" + $lat + " data-lon=" + $lon + " data-lp=" + $lp + " data-lt=" + $lt + " data-pc=" + $pc + ">" + $htmlText + "</option>");
-
-        });
-
-      });
-
-      $searchLocation.change(function() {
-
-        var val = $(this).val();
-
-        var lat = $searchLocationDatalist.find("option").filter(function() {
-
-          return this.value == val;
-
-        }).data("lat");
-
-        var lon = $searchLocationDatalist.find("option").filter(function() {
-
-          return this.value == val;
-
-        }).data("lon");
-
-        var lp = $searchLocationDatalist.find("option").filter(function() {
-
-          return this.value == val;
-
-        }).data("lp");
-
-        var lt = $searchLocationDatalist.find("option").filter(function() {
-
-          return this.value == val;
-
-        }).data("lt");
-
-        $(this).attr({
-
-          "data-lon": lon,
-          "data-lat": lat,
-          "data-lp": lp,
-          "data-lt": lt
-
-        });
 
       });
 
