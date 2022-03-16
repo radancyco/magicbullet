1db921bbccbe6c3edb54ba73c04f164e04576b13
Michael Spellacy (Spell) <spell@spellacy.net>
2022-03-16 15:53:22 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -509,7 +509,7 @@
 
           $(".data-form input, .data-form select").each(function() {
 
-            if($(this).hasClass("input-validation-error")) {
+            if($(this).not(".input-validation-error")) {
 
               $(this).attr("aria-invalid", "true");
 
