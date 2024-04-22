1d27e04bfecb600d9d1502ea225df260a3ca92f0
Michael Spellacy (Spell) <spell@spellacy.net>
2024-04-22 18:14:03 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -542,7 +542,7 @@
             var selectedButtonIndex = Array.from(btnSearchFilter).indexOf(this);
             var remainingButtonIndex = btnSearchFilter.length;
 
-            console.log("selected index" + selectedButtonIndex + " remaining button index" + remainingButtonIndex);
+            console.log("selected index: " + selectedButtonIndex + " remaining button index: " + remainingButtonIndex);
 
             if (remainingButtonIndex) {
 
