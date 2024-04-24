24ada3780c05da02f1dcdb766baff0da03a1baa0
Michael Spellacy (Spell) <spell@spellacy.net>
2024-04-24 09:29:39 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -364,7 +364,7 @@ function initA11yRepair() {
         span.setAttribute('aria-hidden', 'true');
         span.textContent = '*';
 
-        label.appendChild(span);
+       // label.appendChild(span);
 
       });
 
@@ -380,8 +380,8 @@ function initA11yRepair() {
         selectedRegion.setAttribute("role", "region");
         selectedRegion.setAttribute("aria-label", "Selected Job Alerts");
       
-        selected.parentNode.insertBefore(selectedRegion, selected);
-        selectedRegion.appendChild(selected);
+      //  selected.parentNode.insertBefore(selectedRegion, selected);
+     //   selectedRegion.appendChild(selected);
 
       });
 
