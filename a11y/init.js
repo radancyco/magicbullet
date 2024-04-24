d3c08bcee57dc091640c83e31c766adc77d01be2
Michael Spellacy (Spell) <spell@spellacy.net>
2024-04-24 11:54:57 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -12,66 +12,46 @@ console.log("test");
   
 function initA11yRepair() {
 
-  
-  // var a11yRepairExecuted = false;
-
-  // Create a new MutationObserver instance
-
   var a11yObserver = new MutationObserver(function() {
 
-    //    if(!a11yRepairExecuted) {
-    
-          // Clear the previous timeout
-      
-          clearTimeout(a11yObserver.timeout);
-      
-          // Set a timeout to run after NN milliseconds of no mutations
-      
-          a11yObserver.timeout = setTimeout(function() {
+    clearTimeout(a11yObserver.timeout);
 
-            console.log("%c MagicBullet: Accessibility Patch v1.8 in use. ", "background: #6e00ee; color: #fff");
+    a11yObserver.timeout = setTimeout(function() {
 
-    var dataForms = document.querySelectorAll(".data-form");
+      console.log("%c MagicBullet: Accessibility Patch v1.8 in use. ", "background: #6e00ee; color: #fff");
 
-    dataForms.forEach(function(form){
+      var dataForms = document.querySelectorAll(".data-form");
 
-      var keySelected = form.querySelectorAll('.keyword-selected');
+      dataForms.forEach(function(form){
 
-      keySelected.forEach(function(selected) {
+        var keySelected = form.querySelectorAll('.keyword-selected');
+
+        keySelected.forEach(function(selected) {
 
         var selectedRegion = document.createElement("div");
         selectedRegion.classList.add("keyword-region");
         selectedRegion.setAttribute("role", "region");
         selectedRegion.setAttribute("aria-label", "Selected Job Alerts");
-      
-       selected.parentNode.insertBefore(selectedRegion, selected);
-       selectedRegion.appendChild(selected);
+
+        selected.parentNode.insertBefore(selectedRegion, selected);
+        selectedRegion.appendChild(selected);
 
       });
 
-
     });
 
-    }, 800); 
-
-  );
+  }, 800); 
           
   var config = { childList: true, subtree: true };
-          
   a11yObserver.observe(document.body, config);
 
 }
-
 
 function loadA11yPatch(url, callback) {
 
   var a11yBody = document.body;
 
-  // Add A11y hook for implementation team. May come in handy.
-
   a11yBody.classList.add("magicbullet-a11y");
-
-  // Install Language Pack.
 
   var componentLanguagePack = document.createElement("script");
 
@@ -80,8 +60,6 @@ function loadA11yPatch(url, callback) {
   componentLanguagePack.onreadystatechange = callback;
   componentLanguagePack.onload = callback;
 
-  // Only load one language pack per page.
-
   var getComponentLanguagePack = document.getElementById("component-library-language-pack");
 
   if(getComponentLanguagePack === null) {
@@ -89,8 +67,6 @@ function loadA11yPatch(url, callback) {
     document.getElementsByTagName("head")[0].appendChild(componentLanguagePack);
 
   }
-
-  // Run the function after content stops changing
   
   initA11yRepair();
 
