bd283ed5e634c1a73a339a9308ac467b54ebc6d2
Michael Spellacy (Spell) <spell@spellacy.net>
2024-08-12 17:17:32 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -81,7 +81,7 @@ loadA11yPatch("https://services.tmpwebeng.com/component-library/language-pack.js
 
   // MindReader Observers 
 
-  var mindReaderNodes = document.querySelectorAll(".mindreader-status, .typeahead");
+  var mindReaderNodes = document.querySelectorAll(".mindreader-status, #wrapper-sel-city");
 
   function callback(mutationList, mindReaderObserver) {
  
