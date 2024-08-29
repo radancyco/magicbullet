f5e67784affe4214273205ce7812267cf8c9b0e9
Michael Spellacy (Spell) <spell@spellacy.net>
2024-08-29 12:34:48 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -76,7 +76,7 @@ loadA11yPatch("https://services.tmpwebeng.com/component-library/language-pack.js
   a11yObserver.observe(targetNode, config);
 
   initStaticPatch();
-  initDynamicPatch();
+  initDynamicPatch(); 
 
   // MindReader Observers 
 
