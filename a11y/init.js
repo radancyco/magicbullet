2bc092aa617aaac86c544322ac4c7747c18881d0
Michael Spellacy (Spell) <spell@spellacy.net>
2024-09-13 12:57:57 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -664,7 +664,7 @@ function fixMindReaderInput() {
     input.setAttribute("role", "combobox");
     input.setAttribute("aria-controls", input.getAttribute("id") + "-mindreader");
 
-    input.addEventListener("change", function() {
+    input.addEventListener("keyup", function() {
 
       // Fix: When combobox is opened, the input needs to indicate it is opened and remove last active descendent.
 
