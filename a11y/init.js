631414e9a3b9a75538a8e8a08302d0a4fd699f9b
Michael Spellacy (Spell) <spell@spellacy.net>
2024-12-02 17:46:21 -0500

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -662,6 +662,8 @@ function fixGlobalDisclosure() {
 
     button.addEventListener("click", function() {
 
+      var isExpanded = this.getAttribute("aria-expanded") === "true";
+
       this.setAttribute("aria-expanded", isExpanded ? "false" : "true");
 
       // Fix: Remove aria-expanded, aria-hidden being added to adjacent, non-interactive element, by CS Core.
