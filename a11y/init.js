1f571c00df5f14bbaf135e5f76720e91468076be
Michael Spellacy (Spell) <spell@spellacy.net>
2025-09-02 15:15:41 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -68,7 +68,7 @@ loadA11yPatch("https://services.tmpwebeng.com/component-library/language-pack.js
 
   // Create a new MutationObserver instance
 
-  // Note: Instead of having an observer to watch _everything_ in main (#content), consider setting up multiple observers per component.
+  // TODO: Instead of having an observer to watch _everything_ in main (#content), consider setting up multiple observers per component.
   // This might be better and less prone to creating interference on site. Similar to MindReaders seperate observer.
 
   var targetNode = document.querySelector("#content");
