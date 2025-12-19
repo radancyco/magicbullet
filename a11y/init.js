071f9c1c8e893353af2f13aba25bf75b2c0bb910
Michael Spellacy (Spell) <spell@spellacy.net>
2025-12-19 12:23:23 -0500

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -1198,6 +1198,7 @@ function fixSaveJobButton() {
 
     observer.observe(recentlyViewedParent, {
   
+      subtree: true,
       attributes: true,
       attributeFilter: ["data-recently-viewed-jobs"]
 
