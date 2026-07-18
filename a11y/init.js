06cc4b26e60856ddadc913cea8845d4a073802fa
Michael Spellacy (Spell) <spell@spellacy.net>
2026-07-17 20:10:25 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -1631,9 +1631,14 @@ function fixSearchResults() {
 
     // Recalculate count each time the DOM changes
 
-    const searchResultTotal = searchResultsContainer.dataset.totalResults;
-    const searchResultCount = searchResultsContainer.querySelectorAll("#search-results-list > ul > li").length;
-    ariaMsg.textContent = searchResultCount + " of " + searchResultTotal + " results are now available.";
+    setTimeout(function() {
+
+      const searchResultTotal = searchResultsContainer.dataset.totalResults;
+      const searchResultCount = searchResultsContainer.querySelectorAll("#search-results-list > ul > li").length;
+      ariaMsg.textContent = searchResultCount + " of " + searchResultTotal + " results are now available.";
+
+    }, 1000);
+
 
   });
 
