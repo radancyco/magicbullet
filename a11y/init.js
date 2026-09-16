f0fd229b5ac0c11809232a1cdc25ae6899dcd52d
Michael Spellacy (Spell) <spell@spellacy.net>
2026-09-16 11:52:02 -0400

    Update init.js

--- a11y/init.js
+++ a11y/init.js
@@ -39,9 +39,9 @@ function loadA11yPatch(url, callback) {
 
 }
 
-var languagePackSrc = "https://services.tmpwebeng.com/magicbullet/language-pack.js";
+// var languagePackSrc = "https://services.tmpwebeng.com/magicbullet/language-pack.js";
 
-// var languagePackSrc = "https://radancy.dev/magicbullet/language-pack.js";
+var languagePackSrc = "https://radancy.dev/magicbullet/language-pack.js";
 
 loadA11yPatch(languagePackSrc, function(){
 
